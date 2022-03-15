bcebeadffd60ad518447e2f9afe3c80d008b804e
Brian Johnson <johnsonb@objectcomputing.com>
2022-03-15 13:26:51 -0700

    Missed cleanup.

--- include/fc/io/persistence_util.hpp
+++ include/fc/io/persistence_util.hpp
@@ -56,52 +56,6 @@ namespace persistence_util {
       dat_content.write( reinterpret_cast<const char*>(&current_version), sizeof(current_version) );
       return dat_content;
    }
-}
-
-
-inline cfile cfile::read_dat_file(const fc::path& dir, const std::string& filename, const uint32_t magic_number,
-   const uint32_t min_supported_version, const uint32_t max_supported_version) {
-   if (!fc::is_directory(dir))
-      fc::create_directories(dir);
-   
-   auto dat_file = dir / filename;
-   using cfile_stream = fc::datastream<fc::cfile>;
-   cfile_stream dat_content;
-   dat_content.set_file_path(dat_file);
-   dat_content.open(cfile::update_rw_mode);
-
-   // validate totem
-   uint32_t totem = 0;
-   fc::raw::unpack( dat_content, totem );
-   if( totem != magic_number) {
-      FC_THROW_EXCEPTION(fc::parse_error_exception,
-                         "File '${filename}' has unexpected magic number: ${actual_totem}. Expected ${expected_totem}",
-                         ("filename", dat_file.generic_string())
-                         ("actual_totem", totem)
-                         ("expected_totem", magic_number));
-   }
-
-   // validate version
-   uint32_t version = 0;
-   fc::raw::unpack( dat_content, version );
-   if( version < min_supported_version || version > max_supported_version) {
-      FC_THROW_EXCEPTION(fc::parse_error_exception,
-                         "Unsupported version of file '${filename}'. "
-                         "Version is ${version} while code supports version(s) [${min},${max}]",
-                         ("filename", dat_file.generic_string())
-                         ("version", version)
-                         ("min", min_supported_version)
-                         ("max", max_supported_version));
-   }
-   return dat_content;
-}
+} // namespace persistence_util
 
 } // namespace fc
-
-#ifndef _WIN32
-#undef FC_FOPEN
-#else
-#undef FC_CAT
-#undef FC_PREL
-#undef FC_FOPEN
-#endif
